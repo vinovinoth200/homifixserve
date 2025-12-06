0d0d2aa92a1504fef15db42198aa7955c8adeb43
vinoth <vinothagan2004@gmail.com>
2025-12-06 22:13:57 +0530

    second commit

--- src/pages/areas.jsx
+++ src/pages/areas.jsx
@@ -10,7 +10,7 @@ export default function Areas(){
                     <div className="areas-grid">
 
                         <a href="/services" className="a-card area-card">
-                            <img src="src/assets/thanjavur-city.JPG" alt="thanjavur-city" className="area-img"/>
+                            <img src="src/assets/thanjavur-city.jpg" alt="thanjavur-city" className="area-img"/>
                          <div className="area-info">
                               <h3>Thanjavur city</h3>
                               <p>If you are staying in Thanjavur main city,Prefer to select this.</p>   
@@ -18,7 +18,7 @@ export default function Areas(){
                             
                         </a>
                         {/* <Link to="/services" className="a-card area-card">
-                            <img src="src/assets/thanjavur-city.JPG" alt="thanjavur-city" className="area-img"/>
+                            <img src="src/assets/thanjavur-city.jpg" alt="thanjavur-city" className="area-img"/>
                          <div className="area-info">
                               <h3>Thanjavur city</h3>
                               <p>If you are staying in Thanjavur main city,Prefer to select this.</p>   
@@ -27,7 +27,7 @@ export default function Areas(){
                         </Link> */}
 
                         <a href="/services" className="a-card area-card">
-                            <img src="src/assets/big-temple.JPG" alt="big-temple" className="area-img"/>
+                            <img src="src/assets/big-temple.jpg" alt="big-temple" className="area-img"/>
                             <div className="area-info">
                               <h3>Big temple Areas</h3>
                               <p>If you are in this area,You can choose this.</p>
@@ -36,7 +36,7 @@ export default function Areas(){
                         </a>
 
                         <a href="/services" className="a-card area-card">
-                            <img src="src/assets/old-busstand.JPG" alt="old-busstand" className="area-img"/>
+                            <img src="src/assets/old-busstand.jpg" alt="old-busstand" className="area-img"/>
                             <div className="area-info">
                               <h3>Old Bus Stand</h3>
                               <p>If you are in this area,You can choose this.</p>
@@ -45,7 +45,7 @@ export default function Areas(){
                         </a>
 
                         <a href="/services" className="a-card area-card">
-                            <img src="src/assets/vallam.JPG" alt="vallam" className="area-img"/>
+                            <img src="src/assets/vallam.jpg" alt="vallam" className="area-img"/>
                             <div className="area-info">
                               <h3>Vallam</h3>
                               <p>If you are in this area,You can choose this.</p>   
@@ -54,7 +54,7 @@ export default function Areas(){
                         </a>
 
                         <a href="/services" className="a-card area-card">
-                            <img src="src/assets/thiruvaiyaru.JPG" alt="thiruvaiyaru" className="area-img"/>
+                            <img src="src/assets/thiruvaiyaru.jpg" alt="thiruvaiyaru" className="area-img"/>
                             <div className="area-info">
                              <h3>Thiruvaiyaru</h3>
                              <p>If you are in this area,You can choose this.</p>   
@@ -63,7 +63,7 @@ export default function Areas(){
                         </a>
 
                         <a href="/services" className="a-card area-card">
-                            <img src="src/assets/nadukadai.JPG" alt="nadukkadai" className="area-img"/>
+                            <img src="src/assets/nadukadai.jpg" alt="nadukkadai" className="area-img"/>
                             <div className="area-info">
                               <h3>Nadukkadai</h3>
                               <p>If you are in this area,You can choose this.</p>    
@@ -72,7 +72,7 @@ export default function Areas(){
                         </a>
 
                         <a href="/services" className="a-card area-card">
-                            <img src="src/assets/kandiyur.WEBP" alt="kandiyur" className="area-img" />
+                            <img src="src/assets/kandiyur.jpg" alt="kandiyur" className="area-img" />
                             <div className="area-info">
                               <h3>Kandiyur</h3>
                               <p>If you are in this area,You can choose this.</p>   
@@ -81,7 +81,7 @@ export default function Areas(){
                         </a>
 
                         <a href="/services" className="a-card area-card">
-                            <img src="src/assets/pattukottai.JPG" alt="pattukottai" className="area-img"/>
+                            <img src="src/assets/pattukottai.jpg" alt="pattukottai" className="area-img"/>
                             <div className="area-info">
                               <h3>Pattukkottai</h3>
                               <p>If you are in this area,You can choose this.</p>   
@@ -90,7 +90,7 @@ export default function Areas(){
                         </a>
 
                         <a href="/services" className="a-card area-card">
-                            <img src="src/assets/thirukattupalli.WEBP" alt="thirukattupalli" className="area-img"/>
+                            <img src="src/assets/thirukattupalli.jpg" alt="thirukattupalli" className="area-img"/>
                             <div className="area-info">
                               <h3>Thirukkattupalli</h3>
                              <p>If you are in this area,You can choose this.</p>   
